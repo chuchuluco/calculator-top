1eee3964158a2738592f2a0dbc36e9ee00b27957
chuchuluco <oscarfdez05@gmail.com>
2022-08-19 15:08:02 -0500

    Remove more operators div

--- app.js
+++ app.js
@@ -1,16 +1,47 @@
 function add(a, b) { 
-  return a + b
+  return a + b;
 };
 
 function subtract(a, b) {
-  return a - b
+  return a - b;
 } ;
 
 function multiply(a, b) {
-
+  let num = Number((a * b).toFixed(3))
+  return num
 };
 
 function divide(a, b) {
-  
+  let num = Math.round((a / b) * 100) / 100
+  return num
 };
+
+function clear() {
+}
+
+function backspace() {
+}
+
+function appendNumber(number) {
+}
+
+function chooseOperation(operation) {
+}
+
+function compute() {
+}
+
+function updateDisplay() {
+}
+
+
+
+
+
+
+
+let resultNumbers = null;
+let result = document.querySelector('div.result');
+const buttons = document.getElementById('calc')
+const numbers = buttons.querySelectorAll('.num')
 
